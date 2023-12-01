73769fa0cc3b93749a1f75ec1eb7ef369b07b8cb
Nashwa Elerian <128938444+NashwaElerian@users.noreply.github.com>
2023-12-02 00:43:57 +0200

    Add files via upload

--- Login/login_JS.js
+++ Login/login_JS.js
@@ -2,3 +2,15 @@
 $('#header').load('../Header/header.html');
 // load footer
 $('#footer').load('../Footer/footer.html');
+
+
+var photos = document.getElementsByClassName("photo");
+var currentIndex = 0;
+
+function showNextPhoto() {
+  photos[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % photos.length;
+  photos[currentIndex].classList.add("active");
+}
+
+setInterval(showNextPhoto, 1000); // Change photo every 2 seconds (adjust as needed)
--- home_JS.js
+++ home_JS.js
@@ -3,3 +3,38 @@ $('#header').load('./Header/header.html');
 
 // load footer
 $('#footer').load('./Footer/footer.html');
+
+var photos = document.getElementsByClassName("photo");
+var currentIndex = 0;
+
+function showNextPhoto() {
+  photos[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % photos.length;
+  photos[currentIndex].classList.add("active");
+}
+
+setInterval(showNextPhoto, 1000); // Change photo every 2 seconds (adjust as needed)
+
+
+
+var photos2 = document.getElementsByClassName("photo2");
+var currentIndex2 = 0;
+
+function showNextPhoto2() {
+  photos2[currentIndex2].classList.remove("active");
+  currentIndex2 = (currentIndex2 + 1) % photos2.length;
+  photos2[currentIndex2].classList.add("active");
+}
+
+setInterval(showNextPhoto2, 1000); // Change photo every 2 seconds (adjust as needed)
+
+var photos3 = document.getElementsByClassName("photo3");
+var currentIndex3 = 0;
+
+function showNextPhoto3() {
+  photos3[currentIndex3].classList.remove("active");
+  currentIndex3 = (currentIndex3 + 1) % photos3.length;
+  photos3[currentIndex3].classList.add("active");
+}
+
+setInterval(showNextPhoto3, 1000); // Change photo every 2 seconds (adjust as needed)
