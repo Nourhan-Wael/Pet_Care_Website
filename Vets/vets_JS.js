3788afb867f9a6757ce42cbcf4fac3ba13641c8a
Nashwa Elerian <128938444+NashwaElerian@users.noreply.github.com>
2023-12-02 00:43:32 +0200

    Add files via upload

--- Vets/vets_JS.js
+++ Vets/vets_JS.js
@@ -23,3 +23,17 @@ function showSlides() {
   
   setTimeout(showSlides, 2000); // Change slide every 2 seconds (adjust as needed)
 }
+
+
+
+var photos = document.getElementsByClassName("photo");
+var currentIndex = 0;
+
+function showNextPhoto() {
+  photos[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % photos.length;
+  photos[currentIndex].classList.add("active");
+}
+
+setInterval(showNextPhoto, 1000); // Change photo every 2 seconds (adjust as needed)
+
