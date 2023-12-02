6bba8cb218438460cecce232a68e07d9d2b30367
Mariamgemy <123839497+Mariamgemy@users.noreply.github.com>
2023-12-02 21:59:53 +0200

    Add files via upload

--- Sign up 2/signup2_JS.js
+++ Sign up 2/signup2_JS.js
@@ -2,3 +2,27 @@
 $('#header').load('../Header/header.html');
 // load footer
 $('#footer').load('../Footer/footer.html');
+
+var photos = document.getElementsByClassName("photo");
+var currentIndex = 0;
+
+function showNextPhoto() {
+  photos[currentIndex].classList.remove("active");
+  currentIndex = (currentIndex + 1) % photos.length;
+  photos[currentIndex].classList.add("active");
+}
+
+setInterval(showNextPhoto, 1000); // Change photo every 2 seconds (adjust as needed)
+
+
+
+var photos2 = document.getElementsByClassName("photo2");
+var currentIndex2 = 0;
+
+function showNextPhoto2() {
+  photos2[currentIndex2].classList.remove("active");
+  currentIndex2 = (currentIndex2 + 1) % photos2.length;
+  photos2[currentIndex2].classList.add("active");
+}
+
+setInterval(showNextPhoto2, 1000); // Change photo every 2 seconds (adjust as needed)
