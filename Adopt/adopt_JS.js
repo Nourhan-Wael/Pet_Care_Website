5d9facae98ef4d80bb6ec85e2804f7afdf086d93
yasmeen-roshdy <152481503+yasmeen-roshdy@users.noreply.github.com>
2023-12-12 22:35:00 +0200

    adopt

--- Adopt/adopt_JS.js
+++ Adopt/adopt_JS.js
@@ -11,22 +11,92 @@ $('#footer').load('./Footer/footer.html');
 // }
 
 theSelect = document.getElementById("list1")
-imgm = document.getElementById("c1")
+theSelect2 = document.getElementById("list2")
 
-// theSelect.addEventListener("change",function(event){
-    
-//     if(event.target.value=='Male'){
-//         imgm.style.display="none"
-//     }
+imgm1 = document.getElementById("c1")
+imgm2 = document.getElementById("c2")
+imgm3 = document.getElementById("c3")
+imgm4 = document.getElementById("c4")
 
+imgm5 = document.getElementById("c5")
+imgm6 = document.getElementById("c6")
+imgm7 = document.getElementById("c7")
+imgm8 = document.getElementById("c8")
 
-// })
 
 function change1(){
     if(theSelect.value=='Male'){
-        imgm.style.display="none";
+        imgm1.style.display="block";
+        imgm2.style.display="block";
+        imgm3.style.display="block";
+        imgm4.style.display="block";
+        imgm5.style.display="none";
+        imgm6.style.display="none";
+        imgm7.style.display="none";
+        imgm8.style.display="none";
+
+    }
+    else if(theSelect.value=='Female'){
+            imgm5.style.display="block";
+            imgm6.style.display="block";
+            imgm7.style.display="block";
+            imgm8.style.display="block";
+            imgm1.style.display="none";
+            imgm2.style.display="none";
+            imgm3.style.display="none";
+            imgm4.style.display="none";
+
+    }
+    else if(theSelect.value=='Male/Female'){
+            imgm1.style.display="block";
+            imgm2.style.display="block";
+            imgm3.style.display="block";
+            imgm4.style.display="block";
+            imgm5.style.display="block";
+            imgm6.style.display="block";
+            imgm7.style.display="block";
+            imgm8.style.display="block";
+    }
+
     }
 
 
-}
 
+
+    function change2(){
+        if(theSelect2.value=='Cat'){
+            imgm1.style.display="block";
+            imgm5.style.display="block";
+            imgm2.style.display="block";
+            imgm6.style.display="block";
+            imgm3.style.display="none";
+            imgm4.style.display="none";
+            imgm7.style.display="none";
+            imgm8.style.display="none";
+    
+        }
+        else if(theSelect2.value=='Dog'){
+                imgm3.style.display="block";
+                imgm4.style.display="block";
+                imgm7.style.display="block";
+                imgm8.style.display="block";
+                imgm1.style.display="none";
+                imgm5.style.display="none";
+                imgm2.style.display="none";
+                imgm6.style.display="none";
+    
+        }
+        else if (theSelect2.value=='Cat/Dog')
+        {
+                imgm1.style.display="block";
+                imgm2.style.display="block";
+                imgm3.style.display="block";
+                imgm4.style.display="block";
+                imgm5.style.display="block";
+                imgm6.style.display="block";
+                imgm7.style.display="block";
+                imgm8.style.display="block";
+    
+        }
+    }
+    
