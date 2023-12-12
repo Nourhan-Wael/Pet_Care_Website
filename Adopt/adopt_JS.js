9f9ebd5395d1bec3c431c28cc45d9da0244565c4
yasmeen-roshdy <152481503+yasmeen-roshdy@users.noreply.github.com>
2023-12-12 21:07:49 +0200

    adopt

--- Adopt/adopt_JS.js
+++ Adopt/adopt_JS.js
@@ -3,3 +3,30 @@ $('#header').load('../Header/header.html');
 
 // load footer
 $('#footer').load('./Footer/footer.html');
+
+
+// function getSelectValue(){
+//     var selectValue = document.getElementById("list1").value;
+
+// }
+
+theSelect = document.getElementById("list1")
+ imgm = document.getElementById("c1")
+
+// theSelect.addEventListener("change",function(event){
+    
+//     if(event.target.value=='Male'){
+//         imgm.style.display="none"
+//     }
+
+
+// })
+
+function change1(){
+    if(theSelect.value=='Male'){
+        imgm.style.display="none";
+    }
+
+
+}
+
