aec190548b49d17469a3a2e461f5107d8244a4c6
aya khaled <124468391+Ayakhaled0197@users.noreply.github.com>
2023-12-13 08:35:43 +0200

    Add files via upload

--- Care/care_JS.js
+++ Care/care_JS.js
@@ -1,34 +1,40 @@
 // load header
 $('#header').load('../Header/header.html');
 // scroll with arrows
-var button = document.getElementById('slide');
-button.onclick = function () {
-    var container = document.getElementById('container');
-    sideScroll(container,'right',25,100,10);
-};
+// var button = document.getElementById('slide');
+// button.onclick = function () {
+//     var container = document.getElementById('container');
+//     sideScroll(container,'right',25,100,10);
+// };
 
-var back = document.getElementById('slideBack');
-back.onclick = function () {
-    var container = document.getElementById('container');
-    sideScroll(container,'left',25,100,10);
-};
+// var back = document.getElementById('slideBack');
+// back.onclick = function (){
+//     var container = document.getElementById('container');
+//     sideScroll(container,'left',25,100,10);
+// };
 
-function sideScroll(element,direction,speed,distance,step){
-    scrollAmount = 0;
-    var slideTimer = setInterval(function(){
-        if(direction == 'left'){
-            element.scrollLeft -= step;
-        } else {
-            element.scrollLeft += step;
-        }
-        scrollAmount += step;
-        if(scrollAmount >= distance){
-            window.clearInterval(slideTimer);
-        }
-    }, speed);
+// function sideScroll(element,direction,speed,distance,step){
+//     scrollAmount = 0;
+//     var slideTimer = setInterval(function(){
+//         if(direction == 'left'){
+//             element.scrollLeft -= step;
+//         } else {
+//             element.scrollLeft += step;
+//         }
+//         scrollAmount += step;
+//         if(scrollAmount >= distance){
+//             window.clearInterval(slideTimer);
+//         }
+//     }, speed);
+// }
+
+
+if(document.getElementById("flexhigh").checked){
+    document.getElementById("high").style.display="block";
+    document.getElementById("low").style.display="none";
 }
-
-const checked = document.querySelector(".check-arrow");
-checked.onclick =function(){
-    check.style.setProperty("background-image" , "../images/pet sitter/arrow.svg");
-};
+else if (document.getElementById("flexlow").checked) {
+    document.getElementById("high").style.display="none";
+    document.getElementById("low").style.display="block";
+} 
+console.log(document.getElementById("flexhigh").checked);
