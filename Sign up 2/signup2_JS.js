b0f5fa02c9f259892cb72508e3c00679ffe53776
Mariamgemy <123839497+Mariamgemy@users.noreply.github.com>
2023-12-20 17:29:19 +0200

    Add files via upload

--- Sign up 2/signup2_JS.js
+++ Sign up 2/signup2_JS.js
@@ -26,3 +26,77 @@ function showNextPhoto2() {
 }
 
 setInterval(showNextPhoto2, 1000); // Change photo every 2 seconds (adjust as needed)
+
+
+
+var  usernameTrue = 0 ;
+var  username2True = 0 ;
+
+function form0() {
+
+
+const form = document.getElementById('form');
+const country = document.getElementById('username');
+const location = document.getElementById('username2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidText = username => {
+  const re = /^(([^[a-zA-Z]))$/;
+  return re.test(String(username).toLowerCase());
+}
+
+
+const validateInputs = () => {
+
+
+// Redirect to the home page
+if(usernameTrue == 1 && username2True == 1){
+	window.location.href = "../index.html";
+	}
+
+  
+    const usernameValue = username.value.trim();
+    const username2Value = username2.value.trim();
+    
+
+
+    
+    if(usernameValue === '') {
+      setError(username2, 'Email is required');
+    } else if (!isValidText(usernamelValue)) {
+      setError(username, 'Provide a valid email address');
+    } else {
+      setSuccess(username);
+    }
+    
+    
+    if(username2Value === '') {
+        setError(username2, 'Username is required');
+    } else {
+        setSuccess(username2);
+    }
+}
+}
