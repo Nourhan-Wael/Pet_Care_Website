0c2c0a1d74932a89aa4c10fe13d2b578bdf365c9
Nashwa Elerian <128938444+NashwaElerian@users.noreply.github.com>
2023-12-21 03:20:54 +0200

    Add files via upload

--- Vets/vets_JS.js
+++ Vets/vets_JS.js
@@ -26,18 +26,6 @@ function showSlides() {
 
 
 
-var photos = document.getElementsByClassName("photo");
-var currentIndex = 0;
-
-function showNextPhoto() {
-  photos[currentIndex].classList.remove("active");
-  currentIndex = (currentIndex + 1) % photos.length;
-  photos[currentIndex].classList.add("active");
-}
-
-setInterval(showNextPhoto, 1000); // Change photo every 2 seconds (adjust as needed)
-
- 
 
 
 function vetscheckbox() {
